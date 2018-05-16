020e8144fb16788cc5e4878adb510d82a7304339
hammoush <mhammad1991h@gmail.com>
2018-05-16 09:15:55 +0200

    Opdracht5

---
+++ Opdracht1/mijnjava script is.js
@@ -0,0 +1,13 @@
+var  a = 10;
+if ( a > 9 ){
+    console.log("ja dat klopt");
+}
+for ( var i =0; i <= 9 ; i++){
+    console.log("yes " + i);
+}
+var  mijnauto = {
+    kleur : "bleuw",
+    model: "golf",
+    aantalwielen : "4"
+}
+console.log(mijnauto.aantalwielen);
---
+++ Opdracht2/opdracht2.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerHTML ="Mijn geweldige pagina";
+document.getElementById("alo1").innerHTML ="Ik ben erg blij";
+document.getElementById("vrouw").src = "foto/alo8.jpg";
+document.getElementById("vrouw1").src = "foto/alo9.jpg"
